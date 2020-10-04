26dc55a031cf12c74582d4d6a00fac686b5b979f
Riguel Figueiro <riguel@dkosoftware.com.br>
2020-10-04 09:24:04 -0300

    feat: added status function

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,7 @@
 defmodule ExMon do
   alias ExMon.Player, as: PlayerBuilder
   alias ExMon.Game, as: Game
+  alias ExMon.Game.Status
 
   @computer_player_name  "Robotinik"
 
@@ -10,7 +11,9 @@ defmodule ExMon do
 
   def start_game(player) do
     @computer_player_name
-    |> create_player(:punch, :kick, :heal)
+    |> create_player(:punch, :kick,  :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
