802f2fd1db610f92c32eae4f81a21e21bfd36397
Riguel Figueiro <riguel@dkosoftware.com.br>
2020-10-04 09:09:10 -0300

    feat: added agent to game

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,16 @@
 defmodule ExMon do
   alias ExMon.Player, as: PlayerBuilder
+  alias ExMon.Game, as: Game
 
-  def create_player(move_avg, move_heal, move_rnd, name) do
+  @computer_player_name  "Robotinik"
+
+  def create_player(name, move_avg, move_heal, move_rnd) do
     PlayerBuilder.build(move_avg, move_heal, move_rnd, name)
+  end
+
+  def start_game(player) do
+    @computer_player_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
